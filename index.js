48c4f58b1b3adb87212cab66849b612c6ee648f8
Kevin Jonson <kevin.jonson@redfin.com>
2017-09-09 11:05:48 -0700

    babel and gulp stub

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+'use strict';
+
 var http = require('http');
-var server = http.createServer(function(req, res) {
+var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end("hello world!\n");
 });
 server.listen(3000);
+//# sourceMappingURL=index.js.map
